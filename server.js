2d6407307c2683393f2a4f69c424bf616924c824
sdpoulose <sdpoulose@gmail.com>
2020-11-29 13:18:09 -0500

    added basic folder creation code to server

--- server.js
+++ server.js
@@ -3,3 +3,8 @@
     ## TODO - code server to facilitate creation of folders server side
     ##
 */
+
+const fs = require('fs-extra');
+
+const dir = '/tmp/this/path/does/not/exist';
+fs.ensureDirSync(dir);
