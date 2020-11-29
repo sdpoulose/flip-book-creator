44c879b372e59ccc4d6cd3965963f4f0f38f5ce6
sdpoulose <sdpoulose@gmail.com>
2020-11-29 12:35:54 -0500

    removed snap method

--- mainWindow.js
+++ mainWindow.js
@@ -26,29 +26,3 @@
   var inputNode = document.querySelector('input')
   inputNode.addEventListener('change', playSelectedFile, false)
 })()
-
-var video = document.querySelector('video');
-var canvas = document.querySelector('canvas');
-var context = canvas.getContext('2d');
-var w, h, ratio;
-
-video.addEventListener('loadedmetadata', function () {
-  ratio = video.videoWidth / video.videoHeight;
-  w = video.videoWidth - 100;
-  h = parseInt(w / ratio, 10);
-  canvas.width = w;
-  canvas.height = h;
-}, false);
-
-function snap() {
-  context.fillRect(0, 0, w, h);
-  context.drawImage(video, 0, 0, w, h);
-  var link = document.createElement('a');
-  link.download = 'filename.png';
-  link.href = document.getElementById('canvas').toDataURL()
-  link.click();
-}
-
-function capture() {
-  setInterval(snap, 1500);
-}
