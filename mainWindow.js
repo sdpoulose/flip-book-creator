df62e36397fab612746bb6abe14b6dbb863587e7
sdpoulose <sdpoulose@gmail.com>
2020-11-29 13:07:18 -0500

    removed video playable status

--- mainWindow.js
+++ mainWindow.js
@@ -7,20 +7,15 @@
 (function localFileVideoPlayer() {
   'use strict'
   var URL = window.URL || window.webkitURL
-  var displayMessage = function (message, isError) {
-    var element = document.querySelector('#message')
-    element.innerHTML = message
-    element.className = isError ? 'error' : 'info'
-  }
+
   var playSelectedFile = function (event) {
     var file = this.files[0]
     var type = file.type
     var videoNode = document.querySelector('video')
     var canPlay = videoNode.canPlayType(type)
     if (canPlay === '') canPlay = 'no'
-    var message = 'Can play type "' + type + '": ' + canPlay
     var isError = canPlay === 'no'
-    displayMessage(message, isError)
+
 
     if (isError) {
       return
@@ -32,3 +27,5 @@
   var inputNode = document.querySelector('input')
   inputNode.addEventListener('change', playSelectedFile, false)
 })()
+
+
